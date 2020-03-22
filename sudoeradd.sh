a783c1333b3c7fe43733e463d2e5add62966e161
newtstat <29125616@users.noreply.github.com>
2020-03-23 02:04:56 +0900

    sudoeradd.sh を更新した。

--- sudoeradd.sh
+++ sudoeradd.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 sudoeradd() {(set -e; SDR="${1:?}"; SDRF=/etc/sudoers.d/administrators; ROW="${SDR} ALL=(ALL) NOPASSWD:ALL"; if ! id "${SDR}" 1>/dev/null 2>&1; then sudo useradd -m -s /bin/bash -b /home "${SDR}"; yes "Password.${SDR}" | sudo passwd "${SDR}" 2>/dev/null || true; fi; if ! sudo grep -q "${ROW}" "${SDRF}" 2>/dev/null; then echo "${ROW}" | sudo tee -a "${SDRF}" 1>/dev/null; fi; sudo chmod 0600 "${SDRF}")}
 sudoeradd "$@"
+
+# eval "$(curl -fLRsS https://sh.djeeno.com/sudoeradd.sh)"
+# eval "$(wget -qO- https://sh.djeeno.com/sudoeradd.sh)"
