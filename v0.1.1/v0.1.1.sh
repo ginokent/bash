7c44a612388b06fc3eb984415a9f5b33c0953859
newtstat <29125616@users.noreply.github.com>
2020-08-17 17:37:39 +0900

    wip: v0.1.1

---
+++ v0.1.1/v0.1.1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -E -e -o pipefail
+
+cd "$(dirname "$0")" || exit 1
+
+VERSION=v0.1.1
+URL_BASE=djeeno.github.io/sh
+
+copy_and_overwrite () {
+  rm -rf ./"${1:?}" || true
+  cp -a ../"${1:?}" .
+  find "${1:?}" -type f -print0 | xargs -I{} perl -pe "s|(${URL_BASE:?})/|\1/${VERSION:?}/|g" -i {}
+}
+
+for dir in bashrc common direnv git peco; do
+  copy_and_overwrite "${dir:?}"
+done
