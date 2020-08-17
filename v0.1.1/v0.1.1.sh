c65a292020ec0005a96367622da81adcc52fcccb
newtstat <29125616@users.noreply.github.com>
2020-08-17 19:13:35 +0900

    shellcheck disable=SC2038

--- v0.1.1/v0.1.1.sh
+++ v0.1.1/v0.1.1.sh
@@ -9,7 +9,8 @@ URL_BASE=djeeno.github.io/sh
 copy_and_overwrite () {
   rm -rf ./"${1:?}" || true
   cp -a ../"${1:?}" .
-  find "${1:?}" -type f -print0 | xargs -I{} perl -pe "s|(${URL_BASE:?})/|\1/${VERSION:?}/|g" -i {}
+  # shellcheck disable=SC2038
+  find "${1:?}" -type f | xargs -I{} perl -pe "s|(${URL_BASE:?})/|\1/${VERSION:?}/|g" -i {}
 }
 
 for dir in asdf bashrc brew common direnv git peco; do
