a9fdf40d2b66593694718439c607463b124f8c9a
newtstat <29125616@users.noreply.github.com>
2021-03-11 16:17:09 +0900

    Update vscodeenv/vscodeenv.lock.sh

--- vscodeenv/vscodeenv.lock.sh
+++ vscodeenv/vscodeenv.lock.sh
@@ -6,16 +6,16 @@ set -eu
 vscodeenv install 1.54.1
 
 # Extensions
-code --install-extension Unity.unity-debug@3.0.2 --force
-code --install-extension VisualStudioExptTeam.vscodeintellicode@1.2.11 --force
-code --install-extension bierner.markdown-mermaid@1.9.2 --force
-code --install-extension eamodio.gitlens@11.3.0 --force
-code --install-extension golang.go@0.23.0 --force
-code --install-extension mads-hartmann.bash-ide-vscode@1.11.0 --force
-code --install-extension ms-dotnettools.csharp@1.23.9 --force
-code --install-extension ms-vscode-remote.remote-containers@0.163.2 --force
-code --install-extension ms-vscode-remote.remote-ssh-edit@0.65.1 --force
-code --install-extension ms-vscode-remote.remote-ssh@0.65.1 --force
-code --install-extension ms-vsliveshare.vsliveshare@1.0.3912 --force
-code --install-extension timonwong.shellcheck@0.13.2 --force
-code --install-extension zxh404.vscode-proto3@0.5.3 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension Unity.unity-debug@3.0.2 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension VisualStudioExptTeam.vscodeintellicode@1.2.11 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension bierner.markdown-mermaid@1.9.2 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension eamodio.gitlens@11.3.0 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension golang.go@0.23.0 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension mads-hartmann.bash-ide-vscode@1.11.0 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-dotnettools.csharp@1.23.9 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-vscode-remote.remote-containers@0.163.2 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-vscode-remote.remote-ssh-edit@0.65.1 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-vscode-remote.remote-ssh@0.65.1 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-vsliveshare.vsliveshare@1.0.3912 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension timonwong.shellcheck@0.13.2 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension zxh404.vscode-proto3@0.5.3 --force
