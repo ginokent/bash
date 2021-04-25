3eaf4c234114ea484817c9b5f2024bfb20a03e8e
newtstat <29125616@users.noreply.github.com>
2021-04-25 17:18:59 +0900

    samples/golang/benchmark.bash

---
+++ samples/golang/benchmark.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# 前準備
+mkdir -p /tmp/benchmark
+cd /tmp/benchmark || exit 1
+
+# ベンチマークファイル作成
+tee /tmp/benchmark/benchmark_test.go <<"EOF"
+package benchmark_test
+
+import (
+	"strconv"
+	"testing"
+)
+
+func BenchmarkAppendQuote(b *testing.B) {
+	s := []byte{}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		s = strconv.AppendQuote(s, `\"a\"`)
+	}
+}
+EOF
+
+# ベンチマーク走らせる
+go test -bench . -benchmem -test.run=none -test.benchtime=1000ms
+
+# alloc と free のトレースを見る
+go test -c
+GODEBUG=allocfreetrace=1 ./"$(basename "${PWD}")".test -test.run=none -test.bench=BenchmarkAppendQuote -test.benchtime=1ms > trace.log 2>&1
+
+# EOF
