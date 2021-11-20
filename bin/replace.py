0da066cdc4cd852f71c3cba373291f3a00f91b52
newtstat <29125616+newtstat@users.noreply.github.com>
2021-11-21 07:26:14 +0900

    Update bin/replace.py: add help

--- bin/replace.py
+++ bin/replace.py
@@ -2,10 +2,10 @@
 import argparse,re,sys
 # opts
 p = argparse.ArgumentParser()
-p.add_argument("-r", "--replace", action="store_true")
-p.add_argument("-b", "--before",  dest="before")
-p.add_argument("-a", "--after",   dest="after")
-p.add_argument("-f", "--file",    dest="file")
+p.add_argument("-r", "--replace", action="store_true", help="If this option is enabled, the contents of the file will be rewritten.")
+p.add_argument("-b", "--before",  dest="before", help="Regular expression before replacement.")
+p.add_argument("-a", "--after",   dest="after", help="The string after the replacement.")
+p.add_argument("-f", "--file",    dest="file", help="Target file path.")
 if len(sys.argv) <= 1:
     p.print_help()
     exit(1)
