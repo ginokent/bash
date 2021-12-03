cd9464788bc80b8be70941529dd3b9f070cd0173
newtstat <29125616+newtstat@users.noreply.github.com>
2021-12-03 11:07:36 +0900

    misc

--- lint.sh
+++ lint.sh
@@ -17,13 +17,13 @@ RecCRITICAL  () { test "${REC_SEVERITY:-0}" -gt 600 2>/dev/null || _recLog CRITI
 RecALERT     () { test "${REC_SEVERITY:-0}" -gt 700 2>/dev/null || _recLog ALERT     "$@"; }
 RecEMERGENCY () { test "${REC_SEVERITY:-0}" -gt 800 2>/dev/null || _recLog EMERGENCY "$@"; }
 RecRun       () { RecDEBUG "$ $(for a in "$@"; do if echo "$a" | grep -Eq "[[:blank:]]"; then printf "'%s' " "$a"; else printf "%s " "$a"; fi; done | sed "s/ $//")"; "$@"; }
-RecExec      () { _drt='####D#E#L#I#M#I#T#E#R####' && _all=$({ _result=$("$@") && _rtn=$? || _rtn=$? && echo "${_drt:?}${_result-}" && return ${_rtn:-0}; } 2>&1) && _rtn=$? || _rtn=$? && _lno=$(echo "${_all-}" | sed -n "/${_drt:?}/=") && RecDEBUG "$ $(for a in "$@"; do if echo "$a" | grep -Eq "[[:blank:]]"; then printf "'%s' " "$a"; else printf "%s " "$a"; fi; done | sed "s/ $//")" stdout "$(echo "${_all-}" | tail -n +"${_lno:?}" | sed "s/^${_drt:?}//")" stderr "$(echo "${_all-}" | head -n "${_lno:?}" | grep -v "^${_drt:?}")" && return ${_rtn:-0}; }
+RecRun      () { _drt='####D#E#L#I#M#I#T#E#R####' && _all=$({ _result=$("$@") && _rtn=$? || _rtn=$? && echo "${_drt:?}${_result-}" && return ${_rtn:-0}; } 2>&1) && _rtn=$? || _rtn=$? && _lno=$(echo "${_all-}" | sed -n "/${_drt:?}/=") && RecDEBUG "$ $(for a in "$@"; do if echo "$a" | grep -Eq "[[:blank:]]"; then printf "'%s' " "$a"; else printf "%s " "$a"; fi; done | sed "s/ $//")" stdout "$(echo "${_all-}" | tail -n +"${_lno:?}" | sed "s/^${_drt:?}//")" stderr "$(echo "${_all-}" | head -n "${_lno:?}" | grep -v "^${_drt:?}")" && return ${_rtn:-0}; }
 
 # <<
 RecINFO "\"<\" と文字列が隣接すると HTML タグとして解釈される問題があるため、以下コマンドで、ヒアドキュメントの \"<<\" の後ろにスペースを追加するよう置換します。"
-RecExec bash -c 'git grep -l "<<[^[:space:]]" | grep -v "lint\.sh" | xargs -I{} perl -pe "s/(<<)([^[:space:]])/\1 \2/g" -i {} || true'
+RecRun bash -c 'git grep -l "<<[^[:space:]]" | grep -v "lint\.sh" | xargs -I{} perl -pe "s/(<<)([^[:space:]])/\1 \2/g" -i {} || true'
 
 # httpGet
 RecINFO "httpGet を更新します。"
 # shellcheck disable=SC2016
-RecExec bash -c 'git grep -l -E " ##httpGetDoNotRemoveThisComment##" | grep -Ev "^releases/|lint.sh" | xargs -I {} perl -0pe "s@\n.*##httpGetDoNotRemoveThisComment##@\nhttpGet=\"\\\$( { command -v curl 1>/dev/null \\&\\& printf \"curl -fLRSs\"; } || { command -v wget 1>/dev/null \\&\\& printf \"wget -O- -q\"; } )\" \\&\\& export httpGet; [ \"\\\${httpGet:?\"curl or wget are required\"}\" ] || exit 1; [ \"\\\${EnvIsLoaded:-false}\" = true ] || eval \"\\\$(sh -c \"\\\${httpGet} https://newtstat.github.io/bash/common/\")\" || exit 1  ##httpGetDoNotRemoveThisComment##@g" -i {} || true'
+RecRun bash -c 'git grep -l -E " ##httpGetDoNotRemoveThisComment##" | grep -Ev "^releases/|lint.sh" | xargs -I {} perl -0pe "s@\n.*##httpGetDoNotRemoveThisComment##@\nhttpGet=\"\\\$( { command -v curl 1>/dev/null \\&\\& printf \"curl -fLRSs\"; } || { command -v wget 1>/dev/null \\&\\& printf \"wget -O- -q\"; } )\" \\&\\& export httpGet; [ \"\\\${httpGet:?\"curl or wget are required\"}\" ] || exit 1; [ \"\\\${EnvIsLoaded:-false}\" = true ] || eval \"\\\$(sh -c \"\\\${httpGet} https://newtstat.github.io/bash/common/\")\" || exit 1  ##httpGetDoNotRemoveThisComment##@g" -i {} || true'
