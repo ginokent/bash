6806612e088cc9a44d6b9ae5363c465dfbc44fac
newtstat <29125616+newtstat@users.noreply.github.com>
2022-01-11 19:05:19 +0900

    modified: keys.sh

--- keys.sh
+++ keys.sh
@@ -1,30 +1,31 @@
 #!/bin/sh
-set -e
+set -eu
 export LANG=C LC_ALL=C
 
 httpGet="$( { command -v curl 1>/dev/null && printf "curl -fLRSs"; } || { command -v wget 1>/dev/null && printf "wget -O- -q"; } )" && export httpGet; [ "${httpGet:?"curl or wget are required"}" ] || exit 1; [ "${EnvIsLoaded:-false}" = true ] || eval "$(sh -c "${httpGet} https://newtstat.github.io/bash/common/")" || exit 1  ##httpGetDoNotRemoveThisComment##
 
-printf "\033[34m$(date +%Y-%m-%dT%H:%M:%S%z) [   info] %s\033[0m\n" "env:"
-printf "\033[34m$(date +%Y-%m-%dT%H:%M:%S%z) [   info] %s\033[0m\n" "  PUBLIC_KEYS_HTTP_URL: ${PUBLIC_KEYS_HTTP_URL:-USE_DEFAULT}"
 public_keys_http_url="${PUBLIC_KEYS_HTTP_URL:="https://newtstat.github.io/bash/keys"}"
+RecNotice "Get public keys"
+public_keys=$(RecExec sh -c "${httpGet:?} ${public_keys_http_url:?} | grep ^ssh-")
 
-public_keys=$(sh -c "${httpGet:?} ${public_keys_http_url:?}" | grep ^ssh-)
-test -n "${public_keys:?"public key not found in ${public_keys_http_url:?}"}" || exit 1
+if [ -z "${public_keys-}" ]; then
+  RecCritical "public key not found in ${public_keys_http_url:?}"
+  exit 1
+fi
 
 if [ -f "$HOME/.ssh/authorized_keys" ]; then
+  RecNotice "Append public key to authorized_keys"
   echo "${public_keys}" | while read -r LINE; do
     if ! grep -q "$LINE" "$HOME/.ssh/authorized_keys"; then
-      echo "$LINE" >>"$HOME/.ssh/authorized_keys"
+      RecExec sh -c "echo \"$LINE\" >>\"$HOME/.ssh/authorized_keys\""
     fi
   done
 else
+  RecNotice "Generate authorized_keys by public key"
   tmp_random_chars="tmp$(LANG=C LC_ALL=C tr -dc 0-9A-Za-z </dev/urandom | head -c 13 || true)"
   # sudo apt-get install -y openssh-client
-  echo | ssh-keygen -t rsa -N "" -f "$HOME/.ssh/${tmp_random_chars}" 1>/dev/null 2>&1 \
-    && rm -f "$HOME/.ssh/${tmp_random_chars}" \
-    && echo "${public_keys}" >"$HOME/.ssh/${tmp_random_chars}.pub" \
-    && mv -f "$HOME/.ssh/${tmp_random_chars}.pub" "$HOME/.ssh/authorized_keys"
+  RecExec sh -c "echo | ssh-keygen -t rsa -N \"\" -f \"$HOME/.ssh/${tmp_random_chars}\" 1>/dev/null 2>&1 && rm -f \"$HOME/.ssh/${tmp_random_chars}\" && echo \"${public_keys}\" >\"$HOME/.ssh/${tmp_random_chars}.pub\" && mv -f \"$HOME/.ssh/${tmp_random_chars}.pub\" \"$HOME/.ssh/authorized_keys\""
 fi
 
-printf "\033[34m$(date +%Y-%m-%dT%H:%M:%S%z) [   info] %s\033[0m\n" "==> $HOME/.ssh/authorized_keys <=="
-cat "$HOME/.ssh/authorized_keys"
+RecNotice "Content of ~/.ssh/authorized_keys:"
+RecNotice "$(cat "$HOME/.ssh/authorized_keys")"
