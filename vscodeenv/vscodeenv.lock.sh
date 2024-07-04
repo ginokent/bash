82291cdad482335f0d025931c96e4e85c34fc932
ginokent <29125616+ginokent@users.noreply.github.com>
2024-07-05 03:42:55 +0900

    chore(vscodeenv): make freeze

--- vscodeenv/vscodeenv.lock.sh
+++ vscodeenv/vscodeenv.lock.sh
@@ -3,36 +3,36 @@
 set -eu
 
 # Visual Studio Code
-vscodeenv install 1.82.3
+vscodeenv install 1.86.2
 
 # Extensions
-'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension Dart-Code.dart-code@3.76.1 --force
-'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension Dart-Code.flutter@3.76.0 --force
-'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension DavidAnson.vscode-markdownlint@0.52.0 --force
-'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension DiemasMichiels.emulate@1.6.0 --force
-'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension GitHub.copilot-chat@0.7.1 --force
-'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension GitHub.copilot@1.138.0 --force
-'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension GitHub.remotehub@0.62.0 --force
-'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension GitHub.vscode-pull-request-github@0.72.0 --force
-'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension Gruntfuggly.todo-tree@0.0.226 --force
-'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension VisualStudioExptTeam.intellicode-api-usage-examples@0.2.8 --force
-'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension VisualStudioExptTeam.vscodeintellicode@1.2.30 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension aws-scripting-guy.cform@0.0.24 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension bdavs.expect@1.0.1 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension bierner.markdown-mermaid@1.20.0 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension biomejs.biome@2.3.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension bufbuild.vscode-buf@0.5.1 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension dart-code.dart-code@3.76.1 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension dart-code.flutter@3.76.0 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension davidanson.vscode-markdownlint@0.52.0 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension diemasmichiels.emulate@1.6.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension dotenv.dotenv-vscode@0.28.1 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension dustypomerleau.rust-syntax@0.6.1 --force
-'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension eamodio.gitlens@14.5.1 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension eamodio.gitlens@14.9.1 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension esbenp.prettier-vscode@10.1.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension fastly.vscode-fastly-vcl@2.0.2 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension foxundermoon.shell-format@7.2.5 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension geequlim.godot-tools@1.3.1 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension github.copilot-chat@0.12.2 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension github.copilot@1.138.0 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension github.remotehub@0.62.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension github.vscode-github-actions@0.26.2 --force
-'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension golang.go@0.40.0 --force
-'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension hashicorp.terraform@2.29.0 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension github.vscode-pull-request-github@0.72.0 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension golang.go@0.41.4 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension gruntfuggly.todo-tree@0.0.226 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension hashicorp.terraform@2.30.1 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ionutvmi.path-autocomplete@1.25.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension mads-hartmann.bash-ide-vscode@1.39.0 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension mechatroner.rainbow-csv@3.12.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension modular-mojotools.vscode-mojo@0.4.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-azuretools.vscode-docker@1.28.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-dotnettools.csdevkit@1.0.14 --force
@@ -40,14 +40,17 @@ vscodeenv install 1.82.3
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-dotnettools.vscodeintellicode-csharp@0.1.26 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-dotnettools.vscode-dotnet-runtime@2.0.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-kubernetes-tools.vscode-kubernetes-tools@1.3.15 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-python.debugpy@2024.0.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-python.python@2023.20.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-python.vscode-pylance@2023.11.10 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-vscode-remote.remote-containers@0.315.1 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-vscode.azure-repos@0.38.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-vscode.cmake-tools@1.16.31 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-vscode.cpptools-extension-pack@1.3.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-vscode.cpptools-themes@2.0.0 --force
-'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-vscode.cpptools@1.18.5 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-vscode.cpptools@1.20.5 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-vscode.hexeditor@1.9.12 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-vscode.live-server@0.4.13 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-vscode.makefile-tools@0.7.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-vscode.remote-repositories@0.40.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension ms-vsliveshare.vsliveshare@1.0.5892 --force
@@ -56,9 +59,12 @@ vscodeenv install 1.82.3
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension redhat.vscode-yaml@1.14.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension rust-lang.rust-analyzer@0.3.1740 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension streetsidesoftware.code-spell-checker@3.0.1 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension tekumara.typos-vscode@0.1.18 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension timonwong.shellcheck@0.34.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension twxs.cmake@0.0.17 --force
-'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension usernamehw.errorlens@3.15.0 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension usernamehw.errorlens@3.16.0 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension visualstudioexptteam.intellicode-api-usage-examples@0.2.8 --force
+'/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension visualstudioexptteam.vscodeintellicode@1.2.30 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension visualstudiotoolsforunity.vstuc@0.9.3 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension vsls-contrib.gistfs@0.6.0 --force
 '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --install-extension wholroyd.jinja@0.0.8 --force
